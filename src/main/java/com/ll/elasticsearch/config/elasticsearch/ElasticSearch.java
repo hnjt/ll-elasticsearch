b41b7ac15f251a106c6467371b47bc5c7212e9fd
githubchenyb <44561395+githubchenyb@users.noreply.github.com>
2020-08-28 10:25:10 +0800

    Update ElasticSearch.java

--- src/main/java/com/ll/elasticsearch/config/elasticsearch/ElasticSearch.java
+++ src/main/java/com/ll/elasticsearch/config/elasticsearch/ElasticSearch.java
@@ -1,7 +1,13 @@
 package com.ll.elasticsearch.config.elasticsearch;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +17,18 @@ public class ElasticSearch {
 
     @Bean
     public RestHighLevelClient restHighLevelClient() {
-        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials( AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", ""));//es账号密码（默认用户名为elastic）
+        RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("127.0.0.1", 9200, "http")
-                )
-        );
-        return restHighLevelClient;
+                        new HttpHost("localhost", 9200, "http"))
+                        .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                                httpClientBuilder.disableAuthCaching();
+                                return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                            }
+                        }));
+        return client;
     }
 }
